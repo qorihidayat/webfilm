a9a5ee518f167fa9935e95b3b95bc2f400b08c00
Qori Hidayat <hidayatqori@gmail.com>
2023-11-11 15:39:54 +0700

    delete route React

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
+    <App/>
   </React.StrictMode>
 );
