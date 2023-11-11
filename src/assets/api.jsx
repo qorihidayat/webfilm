61d00b4a20929d58bc83462cffbc59706f056a00
Qori Hidayat <hidayatqori@gmail.com>
2023-11-11 16:27:09 +0700

    menabahkan environment credential

--- src/assets/api.jsx
+++ src/assets/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzFhN2RhODliNjczMGZmZDA2ZGU1ODM1MzZkNmExZCIsInN1YiI6IjY1NDEyZTNlNTc1MzBlMDEyY2Y0ZDYxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z2NGgGAZu-oMZQwnHezVTYtQVmGDME8GHR1H8OnUd3I";
-const apiKey = "ec1a7da89b6730ffd06de583536d6a1d";
+const env = import.meta.env;
+const token = env.VITE_REACT_APP_TOKEN;
+const apiKey = env.VITE_REACT_APP_API_KEY;
 
 export const GetMovieList = async() =>{
 const movieList = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
